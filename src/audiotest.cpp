9af472504f12fbfaa5a52c32a03da3d0056444ed
Thomas Goyne <plorkyeran@aegisub.org>
2010-12-14 19:50:43 -0800

    Move external header include to a precompiled header

--- src/audiotest.cpp
+++ src/audiotest.cpp
@@ -1,46 +1,6 @@
+#include "pre.h"
+
 #include "ffms.h"
-
-#define BOOST_FILESYSTEM_VERSION 3
-#define BOOST_FILESYSTEM_NO_DEPRECATED
-#define WIN32_LEAN_AND_MEAN
-
-#include <boost/algorithm/string.hpp>
-#include <boost/assign/std/set.hpp>
-#include <boost/bind.hpp>
-#include <boost/filesystem.hpp>
-#include <boost/filesystem/fstream.hpp>
-#include <boost/foreach.hpp>
-#include <boost/format.hpp>
-#include <boost/process.hpp>
-#include <boost/program_options.hpp>
-#include <boost/range/adaptors.hpp>
-#include <boost/range/algorithm.hpp>
-#include <boost/shared_ptr.hpp>
-#include <boost/thread.hpp>
-#include <iostream>
-#include <omp.h>
-#include <vector>
-
-using namespace std;
-using namespace boost::assign;
-namespace b = boost;
-namespace a = b::adaptors;
-namespace p = b::process;
-namespace po = b::program_options;
-namespace fs = b::filesystem;
-
-#define foreach BOOST_FOREACH
-
-#ifdef _WIN32
-#include <objbase.h>
-#include <tchar.h>
-typedef po::wcommand_line_parser command_line_parser;
-#else
-#define _T(x) x
-#define _tmain main
-#define _TCHAR char
-typedef po::command_line_parser command_line_parser;
-#endif
 
 class error : public exception {
 	string msg;
