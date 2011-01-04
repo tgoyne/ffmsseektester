37bea9c3f7674ac128ce466c0006367b988d017f
Thomas Goyne <plorkyeran@aegisub.org>
2011-01-04 14:45:25 -0800

    Change allocation algorithm in ptransform to avoid getting stuck on long-running tasks

--- src/ptransform.h
+++ src/ptransform.h
@@ -1,35 +1,39 @@
-#undef min
-
-template<class Value, class OutputIterator, class UnaryOperation, class MutexType>
-void locked_apply(Value const& value, OutputIterator out, UnaryOperation func, MutexType &mutex) {
-	b::lock_guard<MutexType> lock(mutex);
+template<class Value, class OutputIterator, class MutexType>
+void locked_apply(Value const& value, OutputIterator out, MutexType *mutex) {
+	b::lock_guard<MutexType> lock(*mutex);
 	*out = value;
 }
 
 template<class InputIterator, class OutputIterator, class UnaryOperation, class MutexType>
-void ptransform_wrapper(InputIterator begin, InputIterator end, OutputIterator out, UnaryOperation func, MutexType &mutex) {
-	for (; begin != end; ++begin, ++out) {
-		locked_apply(func(*begin), out, func, mutex);
+void ptransform_wrapper(b::function<InputIterator()> itsource, InputIterator end, OutputIterator out, UnaryOperation func, MutexType *mutex) {
+	InputIterator it;
+	while ((it = itsource()) != end) {
+		locked_apply(func(*it), out, mutex);
 	}
 }
 
-template<class RandomAccessRange, class OutputIterator, class UnaryOperation>
-void ptransform(RandomAccessRange const& rng, OutputIterator out, UnaryOperation func) {
-	typedef b::range_difference<RandomAccessRange>::type size_type;
-	typedef b::range_const_iterator<RandomAccessRange>::type InputIterator;
-	size_type count = b::distance(rng);
+template<class InputIterator, class MutexType>
+InputIterator ptransform_next(InputIterator *cur, InputIterator const& end, MutexType *mutex) {
+	b::lock_guard<MutexType> lock(*mutex);
+	if (*cur == end) return end;
+	return (*cur)++;
+}
+
+template<class ForwardRange, class OutputIterator, class UnaryOperation>
+void ptransform(ForwardRange const& rng, OutputIterator out, UnaryOperation func) {
+	typedef b::range_const_iterator<ForwardRange>::type InputIterator;
 	unsigned thread_count = b::thread::hardware_concurrency() * 2;
 	b::thread_group threads;
 
-	InputIterator begin = b::begin(rng);
+	InputIterator it = b::begin(rng);
+	InputIterator end = b::end(rng);
 	b::mutex mutex;
 	for (unsigned i = 0; i < thread_count; ++i) {
-		InputIterator end = begin + min<size_type>(count / thread_count + 1, distance(begin, b::end(rng)));
 		threads.create_thread(
 			b::bind(
 				ptransform_wrapper<InputIterator, OutputIterator, UnaryOperation, b::mutex>,
-				begin, end, out, func, b::ref(mutex)));
-		begin = end;
+				b::protect(b::bind(ptransform_next<InputIterator, b::mutex>, &it, end, &mutex)),
+				end, out, func, &mutex));
 	}
 	threads.join_all();
 }
